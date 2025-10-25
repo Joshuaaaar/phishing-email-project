fe63ff6203dfc467322d17dd08450429ab749524
Joshua Ramnauth <joshua20050710@gmail.com>
2025-10-25 15:55:23 -0400

    a

--- code/model.py
+++ code/model.py
@@ -1,6 +1,8 @@
-from dataSet import x_train_vec , x_test_vec, y_testt , y_train
+from dataSet import x_train_vec , x_test_vec, y_testt , y_train , vec
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score, log_loss
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 cl = LogisticRegression(max_iter=1000) 
 cl.fit(x_train_vec,y_train)
@@ -12,9 +14,17 @@ y_cl_test_pred = cl.predict(x_test_vec)
 
 accur_test = accuracy_score(y_testt, y_cl_test_pred)
 
-print(accur_test)
+#Testing custom input 
 
-print(classification_report(y_testt,y_cl_test_pred))
+new_email_vec = vec.transform(new_email)
+
+prediction = cl.predict(new_email_vec)[0]
+
+#print(prediction)
+
+#print(accur_test)
+
+#print(classification_report(y_testt,y_cl_test_pred))
 
 
-print(confusion_matrix(y_testt,y_cl_test_pred))
+#print(confusion_matrix(y_testt,y_cl_test_pred))
