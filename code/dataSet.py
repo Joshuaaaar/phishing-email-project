3c2c41d5fb7b7054c576cd1276d7db59830037c8
Joshua Ramnauth <joshua20050710@gmail.com>
2025-10-25 15:26:54 -0400

    fixed some stuff and vectorized stuff

--- code/dataSet.py
+++ code/dataSet.py
@@ -1,16 +1,23 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 df = pd.read_csv("code/Phishing_Email.csv")
 
 # Getting X data 
 
-x = df.drop('Email Type', axis=1)
-
+x = df["Email Text"].fillna("")
 # Getting Y data
 y = df['Email Type']
 
 # Splitting between training and testing set
 
-x_train , x_test , y_train, y_testt = train_test_split(x,y,test_size=0.2,random_state=100)
+x_train , x_test , y_train, y_testt = train_test_split(x,y,test_size=0.2,random_state=100 , stratify=y)
+
+# Turning text into numbers 
+
+vec = TfidfVectorizer(stop_words="english")
+x_train_vec = vec.fit_transform(x_train)
+x_test_vec = vec.transform(x_test)
 
--- code/model.py
+++ code/model.py
@@ -1,1 +1,12 @@
-from dataSet import x_train , x_test, y_testt , y_train
+from dataSet import x_train_vec , x_test_vec, y_testt , y_train
+from sklearn.linear_model import LogisticRegression
+
+cl = LogisticRegression(max_iter=1000) 
+cl.fit(x_train_vec,y_train)
+
+
+y_cl_train_pred = cl.predict(x_train_vec)
+y_cl_test_pred = cl.predict(x_test_vec)
+
+
+print(y_cl_train_pred)
