00a60c078e91291a8f39772dc1fba2258dd0926f
Joshua Ramnauth <joshua20050710@gmail.com>
2025-10-25 15:44:55 -0400

    a

--- code/model.py
+++ code/model.py
@@ -1,12 +1,20 @@
 from dataSet import x_train_vec , x_test_vec, y_testt , y_train
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score, log_loss
 
 cl = LogisticRegression(max_iter=1000) 
 cl.fit(x_train_vec,y_train)
 
 
-y_cl_train_pred = cl.predict(x_train_vec)
 y_cl_test_pred = cl.predict(x_test_vec)
 
+# Peformance checking
 
-print(y_cl_train_pred)
+accur_test = accuracy_score(y_testt, y_cl_test_pred)
+
+print(accur_test)
+
+print(classification_report(y_testt,y_cl_test_pred))
+
+
+print(confusion_matrix(y_testt,y_cl_test_pred))
