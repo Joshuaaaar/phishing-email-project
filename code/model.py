f17073939df97f3489a8493e66c9d803d5866abc
Joshua Ramnauth <joshua20050710@gmail.com>
2025-10-26 21:38:01 -0400

    joblib

--- code/model.py
+++ code/model.py
@@ -2,6 +2,8 @@ from dataSet import x_train_vec , x_test_vec, y_testt , y_train , vec
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, roc_auc_score, log_loss
 from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+
 
 
 cl = LogisticRegression(max_iter=1000) 
@@ -13,6 +15,12 @@ y_cl_test_pred = cl.predict(x_test_vec)
 # Peformance checking
 
 accur_test = accuracy_score(y_testt, y_cl_test_pred)
+
+
+# joblib
+
+joblib.dump(cl, "phishing_model.joblib")
+joblib.dump(vec, "vectorized.joblib")
 
 #Testing custom input 
 
